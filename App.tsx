0bb1d663ed5d53a67bcd7d4cbd7213f377d51cad
Jose-Gebrael <jose.gebrayel@isae.edu.lb>
2025-06-06 14:44:37 +0300

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer, LinkingOptions} from '@react-navigation/native';
 import {RootStack} from './src/navigation/navigate';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
@@ -6,6 +6,7 @@ import Toast from 'react-native-toast-message';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {RootStackParamList} from './src/types/navigation.types';
 import {OneSignal} from 'react-native-onesignal';
+import BootSplash from 'react-native-bootsplash';
 // import {LogLevel} from 'react-native-onesignal';
 
 const queryClient = new QueryClient();
@@ -27,6 +28,17 @@ export default function App() {
   //OneSignal.Debug.setLogLevel(LogLevel.Verbose);
   OneSignal.initialize('6cb1ed9a-eb6c-47ec-a915-99eeceb5d2c3');
   OneSignal.Notifications.requestPermission(false);
+
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
 
   return (
     <GestureHandlerRootView>
